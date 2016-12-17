d743fe2283283d3dac82eff6cbc0e6f667f28753
IMYin <sunnyin127@gmail.com>
2016-12-17 10:57:10 +0800

    update the code of mysql.

---
+++ mysql/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE announcement_201612(
+publish_time    CHAR(10),
+code            CHAR(6),
+content         TEXT,
+pdf_url         VARCHAR(500)
+);
+
+CREATE TABLE interpretation201612(
+publish_time    CHAR(10),
+code            CHAR(6),
+content         TEXT,
+pdf_url         VARCHAR(500)
+);
+
