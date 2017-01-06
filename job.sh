e5090b5c073eb290821b59a66f4bebba16445564
IMYin <sunnyin127@gmail.com>
2017-01-06 21:52:48 +0800

    update job

--- job.sh
+++ job.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+export PYTHON_HOME="/home/imyin/anaconda2/bin/python"
+
 mypath="$(cd "$(dirname "$0")"; pwd)"
 cd $mypath
+
+# crawl contents from web.
 cd Scrapy
 python ScrapyInterpretation.py
-
 python ScrapyAnnouncement.py
 
-cd ../Job
-python SelectStrategy.py
+# plot the result
+cd ../Analysis
+python quota_analysis.py >> analysis.log
 
-python SendMail.py
+# send mail
+cd ../Job
+python SendMail.py  >> ../Analysis/analysis.log
+#python SelectStrategy.py
