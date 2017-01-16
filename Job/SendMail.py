8859b1255a202660c0db2ab608c2622330e6da18
IMYin <sunnyin127@gmail.com>
2017-01-17 00:48:04 +0800

    send pdfs

--- Job/SendMail.py
+++ Job/SendMail.py
@@ -15,6 +15,7 @@ from email.header import Header
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 # must use absolute path.
 CONSTANTS_PATH = os.path.dirname(os.getcwd())
@@ -22,10 +23,11 @@ sys.path.append(CONSTANTS_PATH)
 import Constants as cons
 
 today_time = sys.argv[1]
-data_path = os.path.join(cons.PLOT_RESULT, today_time)
-os.chdir(data_path)
+plot_data_path = os.path.join(cons.PLOT_RESULT, today_time)
+pdf_data_path = os.path.join(cons.PDF_DOWNLOADED, today_time)
+os.chdir(plot_data_path)
 COMMASPACE = cons.SPLIT_ITEM1
-for root, dirs, files in os.walk(data_path):
+for root, dirs, files in os.walk(plot_data_path):
     file_names = files
 
 
@@ -41,6 +43,10 @@ def run_send(file_names):
         fp.close()
         msgImage.add_header(u'Content-ID', u'<{}>'.format(item[:6]))
         msg.attach(msgImage)
+
+    part = MIMEApplication(open(pdf_data_path + u".zip", u'rb').read())
+    part.add_header('Content-Disposition', 'attachment', filename=today_time + u".zip")
+    msg.attach(part)
 
     to_addr = cons.TO_ADDR
     from_addr = cons.FROM_ADDR
