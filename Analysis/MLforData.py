ae6b7ded236be2a8cfcd1e8c5bae7e81c6aa102b
IMYin <sunnyin127@gmail.com>
2017-03-03 15:15:43 +0800

    update ML

--- Analysis/MLforData.py
+++ Analysis/MLforData.py
@@ -13,15 +13,3 @@ import tushare as ts
 import numpy as np
 from sklearn.linear_model.logistic import LogisticRegression
 
-def process_data(code):
-    df = ts.get_k_data(retry_count=10)
-    y = np.sign(df['close'] - df['open'])
-    X = df['close', 'open', 'high', 'low', 'volume']
-    return df, X, y
-
-df,X,y = process_data('000382')
-classifier = LogisticRegression()
-classifier.fit(X,y)
-predictions = classifier.predict()
-for i, prediction in enumerate(predictions[-5:]):
-    print 'Prediction: %s. Message: %s' % (prediction, X_test_raw.iloc[i])
