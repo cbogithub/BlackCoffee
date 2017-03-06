1444eaae76fd6475adb5adee7c98e75aa560a5c3
IMYin <sunnyin127@gmail.com>
2017-03-06 14:12:53 +0800

    add retry times,set 10 times,and use multiprocessing do this job.

--- Analysis/download_announ_pdf.py
+++ Analysis/download_announ_pdf.py
@@ -11,6 +11,8 @@ Created on 3/1/17 7:53 PM
 
 import os
 import sys
+import time
+import datetime
 from multiprocessing.dummy import Pool as ThreadPool
 from urllib import urlretrieve
 
@@ -34,24 +36,33 @@ def get_announ_codes():
             x = cursor.execute(sql)
             result = cursor.fetchmany(x)
             codes = {item['content']: item[u'pdf_url']
-                                   + cons.SPLIT_ITEM5
-                                   + item[u'content'] for item in result}
+                                      + cons.SPLIT_ITEM5
+                                      + item[u'content'] for item in result}
     finally:
         connection.close()
         return codes
 
-def download_it(info):
+
+def download_it(info, retry=10):
     words_announ = announ_codes[info].split(cons.SPLIT_ITEM5)
     an_file_name = words_announ[1]
     an_pdf = words_announ[0]
-    urlretrieve(an_pdf, an_pdf_data_path + u"/" + an_file_name + u".pdf")
+    download_path = an_pdf_data_path + u"/" + an_file_name + u".pdf"
+    for _ in range(retry):
+        time.sleep(0.01)
+        urlretrieve(an_pdf, download_path)
+        if os.path.getsize(download_path) / 1024 > 2.0:
+            break
+
 
 announ_codes = get_announ_codes()
 
-for item in announ_codes:
-    download_it(item)
-# pool = ThreadPool(32)
-# results = pool.map(download_it, announ_codes)
-# pool.close()
-# pool.join()
-
+# for item in announ_codes:
+#     download_it(item)
+time1 = datetime.datetime.now()
+pool = ThreadPool(8)
+results = pool.map(download_it, announ_codes)
+pool.close()
+pool.join()
+time2 = datetime.datetime.now()
+print (u"\nIt costs {} sec to run it.\nToday is {}...".format((time2 - time1).total_seconds(), today_str_Ymd))
