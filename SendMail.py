f553d16f8ac7ec447ae5522b4e09e2d57854161a
IMYin <sunnyin127@gmail.com>
2017-08-24 20:08:38 +0800

    update structure

---
+++ SendMail.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on  12/5/16 10:57 PM
+
+@author: IMYin
+
+@File: SendMail.py
+"""
+import os
+import smtplib
+import sys
+from email.header import Header
+from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import constants as cons
+
+today_time = sys.argv[1]
+today_str_md = sys.argv[2]
+
+plot_data_path = os.path.join(cons.PLOT_RESULT, today_time)
+pdf_data_path = os.path.join(cons.PDF_DOWNLOADED, today_time)
+os.chdir(plot_data_path)
+COMMASPACE = cons.SPLIT_ITEM1
+for root, dirs, files in os.walk(plot_data_path):
+    file_names = files
+
+
+def get_content_dict():
+    connection = cons.conn_mysql()
+    try:
+        with connection.cursor() as cursor:
+            sql = (cons.content_dict.format(cons.inter_table_name, today_str_md, cons.UP))
+            x = cursor.execute(sql)
+            result = cursor.fetchmany(x)
+            # Collect the contents of article.
+            return {item[u'title']: item[u'content'] for item in result}
+    finally:
+        connection.close()
+
+
+def run_send(file_names):
+    dict_contents = get_content_dict()
+    msg = MIMEMultipart()
+    msg[u'Subject'] = Header(u"Today has {} messages.".format(len(file_names)), u'utf-8')
+
+    for item in file_names:
+        # Embed the picture in text.
+        mail_msg = u'<p>{}<br>{}</p><p><img src="cid:{}"></p>'.format(item, dict_contents[item[6:-4]], item)
+        msg.attach(MIMEText(mail_msg, u'html', u'utf-8'))
+        fp = open(item, u'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+        msgImage.add_header(u'Content-ID', u'<{}>'.format(item[:6]))
+        msg.attach(msgImage)
+
+    # Add attachment into e-mail.
+    part = MIMEApplication(open(pdf_data_path + u".zip", u'rb').read())
+    part.add_header('Content-Disposition', 'attachment', filename=today_time + u".zip")
+    msg.attach(part)
+
+    to_addr = cons.TO_ADDR
+    from_addr = cons.FROM_ADDR
+    password = cons.PASSWORD
+    # Send the email via our own SMTP server.
+    s = smtplib.SMTP(cons.SMTP_SERVER, 25)
+    s.login(from_addr, password)  # to login SMTP server.
+    s.sendmail(from_addr, to_addr, msg.as_string())
+    s.quit()
+
+if __name__ == '__main__':
+    run_send(file_names)
---
+++ scrapy_announcement.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on  12/4/16 9:18 AM
+
+@author: IMYin
+
+@File: scrapy_announcement.py
+"""
+import datetime
+import os
+import sys
+import time
+from urllib.parse import urlparse
+
+import numpy as np
+import pandas as pd
+import pymysql.cursors
+
+import Scrapy.Utils as utils
+import constants as cons
+from Logs.JobLogging import JobLogging
+
+URL = cons.RAW_URL_OF_ANNOUNCEMENT
+URL_Net = urlparse(URL).netloc
+URL_SCHEME = urlparse(URL).scheme
+
+
+class ScrapyAnnouncement:
+    # initial log
+    def __init__(self, log_lev='INFO'):
+        date_today = datetime.datetime.now().date()
+        self.log_name = os.path.splitext(os.path.split(sys.argv[0])[1])[0]
+        log_dir = cons.TASK_LOG_PATH
+        self.today = date_today.strftime("%Y%m%d")
+        # log_dir += '/' + self.today
+        if not os.path.isdir(log_dir):
+            try:
+                os.makedirs(log_dir)
+            except:
+                pass
+        mylog = JobLogging(self.log_name, log_dir)
+        mylog.set_level(log_lev)
+        self.log = mylog.get_logger()
+        self.log.info("ScrapyAnnouncement's log create success.")
+
+    def info(self):
+        publish_time = []
+        news_title = []
+        pdf_url = []
+        stock_code = []
+        for item in range(1, 9):
+            data = {"__EVENTTARGET": "pagerQuestion", "__EVENTARGUMENT": str(item)}
+            bsObj = utils.conn_post(URL, data=data)
+            try:
+                content = bsObj.find("ul", {"class": "gg-list"}).find_all("li")
+                time.sleep(np.random.rand(1))
+                for item in content:
+                    try:
+                        con = item.find_all("a")
+                        s_code = item.find("span", {"class": "code"}).text
+                        stock_code.append(s_code.strip("\n"))
+                        pdf_url.append(URL_SCHEME + "://" + URL_Net + "/" + con[1].attrs["href"])
+                        news_title.append(con[1].attrs["title"].encode('utf-8'))
+                        published = item.find("span", {"class": "time"}).text
+                        publish_time.append(published.strip("()"))
+                    except Exception as e:
+                        pass
+            except Exception as e:
+                pass
+
+        announcements = {u'publishtime': publish_time, u'code': stock_code, u'title': news_title, u'pdfurl': pdf_url}
+        df = pd.DataFrame(announcements, columns=[u'publishtime', u'code', u'title', u'pdfurl'])
+
+        return df
+        # Write content to file.csv
+
+        # archive_path = cons.FILE_ARCHIVE + self.today
+        # if not os.path.isdir(archive_path):
+        #     try:
+        #         os.makedirs(archive_path)
+        #     except:
+        #         pass
+        # data_path = os.path.join(archive_path, self.log_name + ".csv")
+        # df.to_csv(data_path)
+        # self.log.info(u"Save data to {} successful.".format(data_path))
+
+    # Use mysql to store information of stock which is crawled by some websites.
+    def insert_to_table(self, df):
+        connection = pymysql.connect(host=cons.mysql_host,
+                                     user=cons.mysql_user,
+                                     password=cons.mysql_passwd,
+                                     db=cons.stock_db,
+                                     charset='utf8',  # set the mysql character is utf-8 !!!
+                                     cursorclass=pymysql.cursors.DictCursor)
+        try:
+            with connection.cursor() as cursor:
+                for index, row in df.iterrows():
+                    sql = cons.insert_announ_table_sql.format(cons.announ_table_name)
+                    cursor.execute(sql, (
+                        row[u'publishtime'], row[u'code'], row[u'title'], row[u'pdfurl']))
+                    self.log.info(
+                        u"Got the '{}, {}, {}, {}' into table: {}".format(row[u'publishtime'], row[u'code'],
+                                                                          row[u'title'].decode('utf-8'),
+                                                                          row[u'pdfurl'], cons.announ_table_name))
+            connection.commit()
+            self.log.info(u"Great job, you got {} rows information　today.".format(len(df)))
+        finally:
+            connection.close()
+
+
+if __name__ == '__main__':
+    time1 = datetime.datetime.now()
+    rawUrl = cons.RAW_URL_OF_INTERPRETATION
+    run = ScrapyAnnouncement()
+    run.insert_to_table(run.info())
+    time2 = datetime.datetime.now()
+    run.log.info(u"It costs {} sec to run it.".format((time2 - time1).total_seconds()))
+    run.log.info(u"-" * 100)
