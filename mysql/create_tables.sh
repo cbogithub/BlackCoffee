383a1430a3482cccafceafa628527d55bf17ed6b
IMYin <sunnyin127@gmail.com>
2017-08-28 20:41:47 +0800

    update mysql

--- mysql/create_tables.sh
+++ mysql/create_tables.sh
@@ -9,6 +9,7 @@ MYSQL=`which mysql`
 database="stock"
 table_annou="announcement"
 table_inter="interpretation"
+table_east_money="east_money_ann"
 passwd="stock"
 host="localhost"
 
@@ -19,21 +20,31 @@ cd $my_path
 ${MYSQL} -u ${database} -p${passwd} -h ${host} <<EOF 
 
 use stock;
-CREATE TABLE IF NOT EXISTS ${table_annou}_${current_time}(
-publish_time    CHAR(10),
-code            CHAR(6),
-content         TEXT,
-pdf_url         VARCHAR(500)
-) DEFAULT CHARSET=utf8;
+#CREATE TABLE IF NOT EXISTS ${table_annou}_${current_time}(
+#publish_time    CHAR(10),
+#code            CHAR(6),
+#content         TEXT,
+#pdf_url         VARCHAR(500)
+#) DEFAULT CHARSET=utf8;
+#
+#CREATE TABLE IF NOT EXISTS ${table_inter}_${current_time}(
+#publish_time    CHAR(12),
+#code            CHAR(6),
+#title           TEXT,
+#content         TEXT,
+#pdf_url         VARCHAR(500)
+#) DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS ${table_inter}_${current_time}(
-publish_time    CHAR(12),
-code            CHAR(6),
-title           TEXT,
-content         TEXT,
-pdf_url         VARCHAR(500)
-) DEFAULT CHARSET=utf8;
-
+CREATE TABLE ${table_east_money}_${current_time}(
+securitycode          VARCHAR(6)    COMMENT '证券代码',
+securityfullname      VARCHAR(30)   COMMENT '证券的全名称',
+noticetitle           VARCHAR(200)  COMMENT '公告的标题',
+noticedate            VARCHAR(10)   COMMENT '公告发布时间',
+securitytype          VARCHAR(10)   COMMENT '证券类型',
+columnname            VARCHAR(50)   COMMENT '公告类型',
+url                   VARCHAR(400)  COMMENT '公告的url地址'
+) COMMENT='表注释'
+DEFAULT CHARSET=utf8;
 exit
 
 EOF
